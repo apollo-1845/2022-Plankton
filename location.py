61bb76759b0fbb3f84fa141c111dc1a61caf99df
sbottingota <saku.bottingota@gmail.com>
2023-02-04 13:48:23 +0000

    Modified location.py to save the ISS's location to a csv file.

--- location.py
+++ location.py
@@ -2,5 +2,16 @@
 
 from orbit import ISS
 
+import pandas as pd
+
+
+def save_to_csv(data, path):
+    pd.DataFrame(data).to_csv(path, header=False)
+
+
+def save_location():
+    save_to_csv(get_location(), "csvTest.csv")
+
+
 def get_location():
     return ISS.location()
