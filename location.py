0214e90a10ed4f46ec924e19bc65d7a61b8e9445
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-02-23 17:15:42 +0000

    Add comments

--- location.py
+++ location.py
@@ -2,23 +2,6 @@
 
 from orbit import ISS
 
-# import csv
-
-
-# def save_to_csv(data, path):
-#     with open("ISSPositions.csv", "a") as f:
-#         writer = csv.writer(f)
-#         writer.writerow(data)
-#
-# def save_location():
-#     save_to_csv(get_location(), "csvTest.csv")
-
-# def convert_to_exif(angle):
-#     """Convert an angle from skyfield's format to (EXIF's (degrees, minutes 10*seconds), is it in reverse direction?)"""
-#     sign, degrees, minutes, seconds = angle.signed_dms()
-#     angle = (degrees, minutes, seconds)
-#     return sign < 0, angle
-
 def get_location():
     """Return (latitude, longitude), units: degrees"""
     loc = ISS.coordinates()
--- photoProcessing.py
+++ photoProcessing.py
@@ -1,6 +1,6 @@
+"""Functions to process images, using OpenCV"""
 import cv2
 import numpy as np
-"""Functions to process images, using OpenCV"""
 
 IMAGE_SHAPE_NP = (1944, 2592, 3)
 COVER_CENTRE = (2592//2, 1944//2)
@@ -9,11 +9,10 @@ COVER_RADIUS = 1944//2
 CAMERA_COVER_MASK = np.zeros(IMAGE_SHAPE_NP[:2], dtype="uint8")
 CAMERA_COVER_MASK = cv2.circle(CAMERA_COVER_MASK, COVER_CENTRE, COVER_RADIUS, 255, -1)
 
-# print("COVER", CAMERA_COVER_MASK.dtype, CAMERA_COVER_MASK.shape)
 def remove_camera_cover(image):
-    # print("IMG", image.dtype, image.shape)
-    return cv2.bitwise_and(image, image, mask=CAMERA_COVER_MASK) # * CAMERA_COVER_MASK
+    """Set the camera cover region of the image to 0 so it reduces the filesize with compression and removes irrelevant data"""
+    return cv2.bitwise_and(image, image, mask=CAMERA_COVER_MASK)
 
 def is_night(image):
-    # print("MAX", np.max(image))
+    """Return True if the image is dark and therefore useless."""
     return np.max(image) < 100
